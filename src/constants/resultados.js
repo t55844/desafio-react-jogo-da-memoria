b25a70f20d0fd63838e808d136f9ef8f1781b314
fredes <ricardofredes@gmail.com>
2022-12-30 16:01:51 -0300

    feat: adiciona dados dos resultados

---
+++ src/constants/resultados.js
@@ -0,0 +1,22 @@
+const resultados = [
+  {
+    titulo: "Excelente",
+    imagem: "kitekat-4.png",
+    min: 75,
+  },
+  {
+    titulo: "Bom",
+    imagem: "kitekat-3.png",
+    min: 50,
+  },
+  {
+    titulo: "Esquecido",
+    imagem: "kitekat-2.png",
+    min: 25,
+  },
+  {
+    titulo: "Amnésia",
+    imagem: "kitekat-1.png",
+    min: 0,
+  },
+];
